52283ca1f9a805cb0a31c522e7785322beef6b84
Cameron Bowie <bowiec@onid.oregonstate.edu>
2015-04-04 00:20:52 -0700

    update command list, add query, abrt, shoot, etc

--- host/driver.py
+++ host/driver.py
@@ -9,36 +9,44 @@ class TurretDriver:
 
 	ZERO_YAW   = 5
 
-	SHOOT      = 6
-	INTIMIDATE = 7
+	SHOOT_ONCE = 6
+	SHOOT_BGN  = 7
+	SHOOT_END  = 8
+	INTIMIDATE = 9
 
-	ENABLE_YAW = 8
-	ENABLE_PCH = 9
-	ENABLE_TRG = 10
+	ENABLE_YAW = 10
+	ENABLE_PCH = 11
 
-	def __init__(self, mailbox=0, host=None, name=None):
-		self.mailbox = mailbox
+	QUERY_YAW  = 12
+	QUERY_PCH  = 13
+
+	ABORT      = 14
+
+	def __init__(self, outbox=0, inbox=1, host=None, name=None):
+		self.outbox = outbox
 		self.brick = nl.find_one_brick(host=host, name=name)
 
 	def _set_yaw_msg(self, yaw):
 		if yaw == None:
 			return ""
+		# else:
+			# return str(SET_YAW) + 
 
 	def zero_yaw(self):
 		pass
-		# self.brick.message_write(self.mailbox, "zero_yaw")
+		# self.brick.message_write(self.outbox, "zero_yaw")
 
 	def set_pos(self, yaw=None, pitch=None):
 		pass
 
-		# self.brick.message_write(self.mailbox, "set_pos:%3d,%3d" % (yaw, pitch))
+		# self.brick.message_write(self.outbox, "set_pos:%3d,%3d" % (yaw, pitch))
 
 	def set_rpos(self, yaw=None, pitch=None):
 		pass
 		
 
-b = nl.find_one_brick()
+# b = nl.find_one_brick()
 
-b.message_write(0, "\0A")
+# b.message_write(0, "\0A")
 
-print 'Test succeeded!'
+# print 'Test succeeded!'
